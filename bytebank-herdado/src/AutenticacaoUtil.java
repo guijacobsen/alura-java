81f7c2b6a45f8890db08b22323c01cf2be02ed96
Guilherme Jacobsen <guijacobsen@gmail.com>
2019-01-03 18:33:25 -0200

    WIP - composicao

---
+++ bytebank-herdado/src/AutenticacaoUtil.java
@@ -0,0 +1,14 @@
+
+public class AutenticacaoUtil implements Autenticavel {
+	
+	private int senha;
+	
+	public void setSenha(int senha) {
+		this.senha = senha;		
+	}
+
+	public boolean autentica(int senha) {
+		return this.senha == senha;
+	}
+
+}
--- bytebank-herdado/src/Gerente.java
+++ bytebank-herdado/src/Gerente.java
@@ -1,7 +1,11 @@
 
 public class Gerente extends Funcionario implements Autenticavel {
-
-	private int senha;
+	
+	private AutenticacaoUtil autenticador;
+	
+	public Gerente() {
+		this.autenticador = new AutenticacaoUtil();
+	}
 	
 	public double getBonificacao() {
 		System.out.println("getBonificacao Gerente");
@@ -10,12 +14,12 @@ public class Gerente extends Funcionario implements Autenticavel {
 
 	@Override
 	public void setSenha(int senha) {
-		this.senha = senha;
+		this.autenticador.setSenha(senha);
 	}
-	
+
 	@Override
 	public boolean autentica(int senha) {
-		return this.senha == senha;
+		return this.autenticador.autentica(senha);
 	}
 	
 }
