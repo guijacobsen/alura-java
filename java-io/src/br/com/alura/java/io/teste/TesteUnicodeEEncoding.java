4ecc43d58317edd4a7d8a9b85eb033a9fc60dbf4
Guilherme Jacobsen <guijacobsen@gmail.com>
2019-01-12 22:50:19 -0200

    WIP - encoding --- codificacao de caracteres (utf-8, windows-1252, utf-16, asc) -- maior loucura

---
+++ java-io/src/br/com/alura/java/io/teste/TesteUnicodeEEncoding.java
@@ -0,0 +1,30 @@
+package br.com.alura.java.io.teste;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public class TesteUnicodeEEncoding {
+
+	public static void main(String[] args) throws UnsupportedEncodingException {
+		
+//		String s = "CaéEÉ;4";
+		String s = "C";
+		System.out.println("string: " + s );
+//		for( int i = 0; i < s.length(); i++ ) {
+//			System.out.println("Char: " + s.charAt(i) + ", CodePoint: " + s.codePointAt(i) );
+//		}
+		
+//		System.out.println( "Charset.defaultCharset() : " + Charset.defaultCharset() );
+//		System.out.println( "Charset.defaultCharset().displayName() : " + Charset.defaultCharset().displayName() );
+		
+//		System.out.println( s.getBytes().length );
+//		System.out.println( s.getBytes("windows-1252").length );
+//		System.out.println( s.getBytes("UTF-8").length );
+//		System.out.println( s.getBytes("UTF-16").length );
+//		System.out.println( s.getBytes( StandardCharsets.ISO_8859_1 ).length );
+//		System.out.println( s.getBytes( StandardCharsets.US_ASCII ).length );
+		
+	}
+	
+}
