06e212a50ab655cec741c02373a7835a21fd2c24
Guilherme Jacobsen <guijacobsen@gmail.com>
2019-02-05 18:01:54 -0200

    [WIP] SERVLET - rodando apache tomcat e criado duas paginas estaticas e uma 'dinamica' com Servlet

---
+++ gerenciador/src/br/com/alura/gerenciador/servlet/OiMundoServlet.java
@@ -0,0 +1,32 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//oi
+@WebServlet(urlPatterns="/oi")
+public class OiMundoServlet extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+		String nome = "Maira";
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<p>Oi mundo.. escrevendo primeiro servlet</p>");
+		out.println("<p>Nome: "+ nome +"</p>");
+		out.println("</body>");
+		out.println("</html>");
+		
+		System.out.println("Servlet OiMundoServlet chamado");
+		
+	}
+	
+}
