033bad6d2571bef9845ebb03af64d95c962a8bc5
Guilherme Jacobsen <guijacobsen@gmail.com>
2019-02-06 15:45:59 -0200

    [WIP] Criado classe para simular banco de dados, classe Empresa e Servlet p/ listagem de empresas

--- gerenciador/src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -2,6 +2,7 @@ package br.com.alura.gerenciador.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,15 +17,46 @@ import javax.servlet.http.HttpServletResponse;
 public class NovaEmpresaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Cadastrando nova empresa");
 		
 		String nomeEmpresa = request.getParameter("nome");
 		
+		Empresa empresa = new Empresa();
+		empresa.setNome(nomeEmpresa);
+		
+		Banco banco = new Banco();
+		banco.adiciona(empresa);
+		
+		List<Empresa> empresas = banco.getEmpresas();
+		
 		PrintWriter out = response.getWriter();
+		
+//		String htmlList = "<ul>";
+//		empresas.forEach(e -> {
+//			htmlList += "<li>";
+//				htmlList += "Nome: " + e;
+//			htmlList += "</li>";
+//		});
+		
+		System.out.println("Empresas : ");
+		System.out.println(empresas);
+		
+		String htmlList = "<ul>";
+		for( Empresa e : empresas ) {
+			System.out.println(e.getNome());
+			htmlList += "<li>";
+				htmlList += "Nome: " + e.getNome();
+			htmlList += "</li>";
+		}
+		htmlList += "</ul>";
+		
+		System.out.println(htmlList);
+		
 		out.println("<html>");
 		out.println("<body>");
 		out.println("Cadatro empresa: " + nomeEmpresa);
+		out.println(htmlList);
 		out.println("</body>");
 		out.println("<html>");
 		
